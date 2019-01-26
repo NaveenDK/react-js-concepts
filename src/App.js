7c7c0e7ec94aef8971cf5a7c3d51994420136153
naveendk <naveenkolambage@gmail.com>
2019-01-27 11:39:38 +1200

    added the two possible ways of passing methods bw components

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ class App extends Component {
 
   }
  
-  switchNameHandler = () =>{
+  switchNameHandler = (newName) =>{
  // console.log('Was clicked')
 //DONOT MUTATE CHANGE STATE DIRECTLY-not recognized this.state.persons[0].name = 'Maximillian';
    this.setState({
     persons:[
-      {name: 'Maximilian', age: 28},
+      {name: newName, age: 28},
       {name:'Manu', age:29},
       {name:'Stephanie', age:289}
 
@@ -31,10 +31,21 @@ class App extends Component {
       <div className="App">
       <h1>Hi, I'm a react App</h1>
       <p> I see me there!</p>
-      <button onClick={this.switchNameHandler}> Switch Name</button>
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-      <Person name={this.state.persons[1].name}  age ={this.state.persons[1].age}> My Hobbies: Racing</Person>
-      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+    {/* <button onClick={this.switchNameHandler.bind(this,'Maxmillian')}> Switch Name</button>*/  }
+     { /*the below method is a bit inefficient and above bind method is recommended */}
+      <button onClick={()=>this.switchNameHandler('Maxmillian!!')}> Switch Name</button>
+      <Person 
+      name={this.state.persons[0].name} 
+      age={this.state.persons[0].age}/>
+      <Person 
+      name={this.state.persons[1].name} 
+      age ={this.state.persons[1].age}
+      click={this.switchNameHandler.bind(this,'Max!')}
+       >My Hobbies: Racing</Person>
+
+      <Person
+       name={this.state.persons[2].name}
+        age={this.state.persons[2].age}/>
       </div>
 
         );
