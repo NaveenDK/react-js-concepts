613072592e696ee5cd3d167ed43d5df2c646ab41
naveendk <naveenkolambage@gmail.com>
2019-01-27 11:00:47 +1200

    added setstate to switch data

--- src/App.js
+++ src/App.js
@@ -13,13 +13,25 @@ class App extends Component {
 
   }
  
+  switchNameHandler = () =>{
+ // console.log('Was clicked')
+//DONOT MUTATE CHANGE STATE DIRECTLY-not recognized this.state.persons[0].name = 'Maximillian';
+   this.setState({
+    persons:[
+      {name: 'Maximilian', age: 28},
+      {name:'Manu', age:29},
+      {name:'Stephanie', age:289}
+
+    ]
+   })
+  }
 
   render() {
     return (
       <div className="App">
       <h1>Hi, I'm a react App</h1>
       <p> I see me there!</p>
-      <button>Switch Name</button>
+      <button onClick={this.switchNameHandler}> Switch Name</button>
       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
       <Person name={this.state.persons[1].name}  age ={this.state.persons[1].age}> My Hobbies: Racing</Person>
       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
