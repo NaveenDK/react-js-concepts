aa1eddeb03ee707ec0e156756cdf095bdfc2ce54
naveendk <naveenkolambage@gmail.com>
2019-01-28 23:01:18 +1200

    added finetuned list-conditionals

--- src/App.js
+++ src/App.js
@@ -2,45 +2,53 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Person from './Person/Person';
+import person from './Person/Person';
 
 class App extends Component {
   state ={
     persons:[
-      {name: 'Max', age: 28},
-      {name:'Manu', age:29},
-      {name:'Stephanie', age:26}
+      {id: '123', name: 'Max', age: 28},
+      {id: '124', name:'Manu', age:29},
+      {id: '125', name:'Stephanie', age:26}
 
     ],
     showPersons :false
 
   }
- 
+ deletePersonHandler = (personIndex) =>{
+ //EVEN THOUGH below line does the job - in JS objects, arrays are reference type therefore after
+ //this line 'persons' can be mutated so its better to use the line which is 'const persons = this.state.persons.splice();//
+ //const persons = this.state.persons;
+ //we cann use spice but in ES6 we are better of using this 3 dots - spread operator
+ const persons = [...this.state.persons];
+ persons.splice(personIndex,1);
+ this.setState({persons:persons})
 
-  switchNameHandler = (newName) =>{
- // console.log('Was clicked')
-//DONOT MUTATE CHANGE STATE DIRECTLY-not recognized this.state.persons[0].name = 'Maximillian';
-   this.setState({
-    persons:[
-      {name: newName, age: 28},
-      {name:'Manu', age:29},
-      {name:'Stephanie', age:289}
+ }
 
-    ],
 
-   })
-  }
+nameChangedHandler = (event,id )=>{
+  
+        const personIndex = this.state.persons.findIndex(p => {
+        return p.id === id;
+        });
+      
+        const person = {
+          ...this.state.persons [personIndex]
+        };
+        //alternative
+        // const person = Object.assign({}, this.stae.persons[personIndex]);
+        
+        person.name = event.target.value;
 
-nameChangedHandler = (event)=>{
-  this.setState({
-    persons:[
-      {name: 'Max', age: 28},
-      {name:event.target.value, age:29},
-      {name:'Stephanie', age:26}
+        const persons = [...this.state.persons];
+        persons[personIndex] = person;
 
-    ]
-   })
+        this.setState({
+          persons:persons
+        });
 
-}
+}//end of namechangedhandler
 
 togglePersonsHandler = ()=>{
  const doesShow = this.state.showPersons;
@@ -63,20 +71,16 @@ let persons= null;
 if (this.state.showPersons){
   persons=(
     <div>
-    <Person 
-    name={this.state.persons[0].name} 
-    age={this.state.persons[0].age}/>
-    <Person 
-    name={this.state.persons[1].name} 
-    age ={this.state.persons[1].age}
-    click={this.switchNameHandler.bind(this,'Max!')}
-    changed={this.nameChangedHandler}
-    >My Hobbies: Racing</Person>
-
-    <Person
-    name={this.state.persons[2].name}
-      age={this.state.persons[2].age}/>
-</div> 
+      {this.state.persons.map((person, index) => {
+        return <Person
+            click ={()=>this.deletePersonHandler(index)}
+            name={person.name}
+            age={person.age}
+            key={person.id}
+            changed ={(event)=> this.nameChangedHandler(event,person.id)}
+            />
+      })}
+    </div> 
 
   )
 }
