52066913845db40544abcd1a898ea023752abf9e
naveendk <naveenkolambage@gmail.com>
2019-02-03 19:27:32 +1200

    modified personjs and personjs to stateful - classes/components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React,{Component} from 'react';
 
 import classes from './Person.css';
-const person=(props) =>{
+ 
+class Person extends Component {
+    render(){
+        return(
+
+            <div className={classes.Person}>
+                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+
+                    <p>{this.props.children}</p>
+                    <input type="text" onChange={this.props.changed} value={this.props.name}/>
+
+
+            </div>
+        )
+    }
+}
+
+
+
+export default Person;
+
+/*const person=(props) =>{
    
     return (
     <div className={classes.Person} >
@@ -12,5 +33,4 @@ const person=(props) =>{
 
     </div>)
 };
-
-export default person;
+*/
